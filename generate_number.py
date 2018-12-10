2e5aec63e5c6777bc9f2dd139e152c3462a77c50
Ali-E-Gokce <38869714+Ali-E-Gokce@users.noreply.github.com>
2018-12-10 12:46:20 -0500

    Update generate_number.py

--- generate_number.py
+++ generate_number.py
@@ -6,7 +6,7 @@ def generate(number_length): #generates random number that fits bagel rules
   
   number=first*(10**(number_length-1)) #uses algebra to generate integer instead of string
 
-  for i in range(len(second_list)):
+  for i in range(len(second_list)): #each number can be represented as a*10^(amount_of_digits-1)+b*10(amount_of_digits-2)...+c*10(amount_of_digits-amount_of_digits)
     number+=second_list[i]*(10**(number_length-i-2))
 
   return number
